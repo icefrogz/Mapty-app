753b0cb37892091082649647bda17eca73706fc8
Ignatius Yudhistira <yudhistiraignatius@gmail.com>
2024-06-05 11:03:21 +1000

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert('could not get your position');
+    }
+  );
+
+const map = L.map('map').setView([-37.82524364433162, 144.9636935573586], 13);
+L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+var marker = L.marker([-37.82524364433162, 144.9636935573586])
+  .addTo(map)
+  .bindPopup('A pretty CSS3 popoup. <br>easily customisable')
+  .openPopup();
